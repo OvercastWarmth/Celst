c06daffbc3f4f497f1bd0bbf06bc5e32a43e1de6
Ringlings <110815694+Ringlings@users.noreply.github.com>
2022-11-08 16:30:34 +1000

    also these whitespace changes from mantaylia that intellij didnt let me add to the previous commit for some reason

--- src/main/java/io/github/ringlings/celestemod/CelesteMod.java
+++ src/main/java/io/github/ringlings/celestemod/CelesteMod.java
@@ -8,9 +8,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CelesteMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod name as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+	//	This logger is used to write text to the console and the log file.
+//	It is considered best practice to use your mod name as the logger's name.
+//	That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("CelesteMod");
 
 	@Override
